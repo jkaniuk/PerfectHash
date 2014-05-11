580850afca6998e2e56f8dccaf28ba9c79c55bf5
Jacek_FH <jacekfh@gmail.com>
2014-05-11 23:38:04 +0200

    memory tests

--- tests/test_PerfectHash.py
+++ tests/test_PerfectHash.py
@@ -41,28 +41,39 @@ class BigTest(unittest.TestCase):
         self.assertEqual(len(self.data), len(self.phash))
         self.assertEqual(len(self.data), self.count)
 
-    def test_size(self):
+    def test_memory(self):
         phash_size = self.phash.t.nbytes + self.phash.g.nbytes
         self.assertGreater(sys.getsizeof(self.data), 3 * phash_size)
 
 
 class MultipleTest(unittest.TestCase):
-    counts_small = [1, 2, 3, 4, 5]
-    counts_big = [7, 10, 20, 50, 100, 200, 500, 1000]
+    counts = [7, 10, 20, 50, 100, 200, 500, 1000]
+    data = {i: DataSource.get_random(i) for i in counts}
+    phashes = {i: PerfectHash(d) for i, d in data.iteritems()}
+
+    def test_len(self):
+        for count in self.counts:
+            self.assertEqual(len(self.data[count]), len(self.phashes[count]))
 
     def test_equal(self):
-        for count in self.counts_big:
-            data = DataSource.get_random(count)
-            phash = PerfectHash(data)
-            self.assertEqual(len(data), len(phash))
-            for key, val in data.iteritems():
-                self.assertEqual(val, phash[key])
+        for count in self.counts:
+            for key, val in self.data[count].iteritems():
+                self.assertEqual(val, self.phashes[count][key])
+
+    def test_memory(self):
+        for count in self.counts:
+            phash = self.phashes[count]
+            data = self.data[count]
             phash_size = phash.t.nbytes + phash.g.nbytes
             self.assertGreater(sys.getsizeof(data), 3 * phash_size)
 
+
+class BruteTest(unittest.TestCase):
+    counts = [1, 2, 3, 4, 5]
+
     def test_brute(self):
         for i in xrange(100):
-            for count in self.counts_small:
+            for count in self.counts:
                 data = DataSource.get_random(count)
                 phash = PerfectHash(data)
                 self.assertEqual(len(data), len(phash))
